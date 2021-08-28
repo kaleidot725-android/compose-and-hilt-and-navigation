401c0a29204ecfe29dfad08b8d60b5bcf928d5a7
Yusuke Katsuragawa <hakodate.katsuragawa.yusuke@gmail.com>
2021-08-28 18:46:14 +0900

    feat: update screen

--- app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -6,21 +6,32 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import jp.kaleidot725.sample.ui.screen.OneScreen
+import jp.kaleidot725.sample.ui.screen.TwoScreen
 import jp.kaleidot725.sample.ui.theme.SampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             SampleTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    NavHost(navController, startDestination = "one") {
+                        composable("one") {
+                            OneScreen()
+                        }
+                        composable("two") {
+                            TwoScreen()
+                        }
+                    }
                 }
             }
         }
