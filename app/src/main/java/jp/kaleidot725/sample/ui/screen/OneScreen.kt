042e35aaf6e4bc33b0ef12f950b94a9e4f0f4816
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-08-28 17:18:07 +0900

    feat: update

---
+++ app/src/main/java/jp/kaleidot725/sample/ui/screen/OneScreen.kt
@@ -0,0 +1,16 @@
+package jp.kaleidot725.sample.ui.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OneScreen() {
+    Text("ONE SCREEN")
+}
+
+@Preview
+@Composable
+private fun OneScreen_Preview() {
+    OneScreen()
+}
---
+++ app/src/main/java/jp/kaleidot725/sample/ui/screen/TwoScreen.kt
@@ -0,0 +1,16 @@
+package jp.kaleidot725.sample.ui.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TwoScreen() {
+    Text("TWO SCREEN")
+}
+
+@Preview
+@Composable
+private fun TwoScreen_Preview() {
+    TwoScreen()
+}
